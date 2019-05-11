a18d603d6a4eb1cec27139a25f6876ef7dfb8784
Tanpero <3185750139@qq.com>
2019-05-12 07:05:45 +0800

    update data::ini::INISection

--- src/data/ini/src/data.ini/section.cpp
+++ src/data/ini/src/data.ini/section.cpp
@@ -7,7 +7,38 @@ namespace tfc
 		namespace ini
 		{
 
-			
+			bool INISection::hasKey(std::string key)
+			{
+				return false;
+			}
+
+			size_t INISection::length()
+			{
+				return items.size();
+			}
+
+			std::vector<INIItem> INISection::getItems()
+			{
+				std::vector<INIItem> items;
+				for (INISection::INIItemIterator i = this->begin(); i != this->end(); ++i)
+				{
+					items.push_back(*i);
+				}
+				return items;
+			}
+
+			std::vector<std::pair<std::string, std::string>> INISection::getEntries()
+			{
+				std::vector<std::pair<std::string, std::string>> entries;
+				for (INISection::INIItemIterator i = this->begin(); i != this->end(); ++i)
+				{
+					std::pair<std::string, std::string> entry;
+					entry.first = i->key;
+					entry.second = i->value;
+					entries.push_back(entry);
+				}
+				return entries;
+			}
 		};
 	};
 };
