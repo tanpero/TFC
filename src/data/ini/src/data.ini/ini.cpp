015b003708e8aafd0ecf09d531cc2f4d2b24824e
Tanpero <3185750139@qq.com>
2019-05-12 06:53:14 +0800

    update data::ini::INIFile::getValue

--- src/data/ini/src/data.ini/ini.cpp
+++ src/data/ini/src/data.ini/ini.cpp
@@ -7,6 +7,32 @@ namespace tfc
 		namespace ini
 		{
 
+			std::string INIFile::getValue(std::string section, std::string key)
+			{
+				INISection sect;
+
+				try
+				{
+					sect = getSection(section);
+				}
+				catch (INIException& e)
+				{
+					throw INIException(e.errtype(), e.info());
+				}
+
+				for (INISection::INIItem_it it = sect->begin(); it != sect->end(); ++it) {
+					if (it->key == key)
+					{
+						*value = it->value;
+						*comment = it->comment;
+						return RET_OK;
+					}
+				}
+
+				errMsg = std::string("not find the key ") + key;
+				return ERR_NOT_FOUND_KEY;
+			}
+
 
 		};
 	};
